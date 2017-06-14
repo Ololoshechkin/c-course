dcbbcf0fd60b2b7b47f9f5672e0db5aee7fde890
Ololoshechkin <v.brilyantov@yandex.ru>
2017-06-14 20:18:41 +0300

    removing rubbish

--- Vector/Vector/vector.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  vector.cpp
-//  Vector
-//
-//  Created by Vadim on 22.04.17.
-//  Copyright © 2017 Vadim. All rights reserved.
-//
-
-#include "vector.hpp"
--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-	int x = 5;
-	cout << x*x;
-	return 0;
-}
-
