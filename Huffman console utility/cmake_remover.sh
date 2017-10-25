8a7352125d9e4a37e8fbe763ee5e50b85b7d49f1
Ololoshechkin <v.brilyantov@yandex.ru>
2017-10-25 20:07:12 +0300

    cleen-up

--- Huffman console utility/cmake_remover.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-ls
-find . -name "Makefile*"  | xargs git rm
-find . -name "CMakeF*"    | xargs git rm
-find . -name "CMakeC*"    | xargs git rm
-find . -name "cmake*"     | xargs git rm
